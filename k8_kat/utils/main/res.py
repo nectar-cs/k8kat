98ab63703289bddf38c3204138d2937286dfde7c
xavier <xavier@codenectar.com>
2020-01-09 14:46:01 +0100

    forgotten files added

---
+++ k8_kat/utils/main/res.py
@@ -0,0 +1,44 @@
+import re
+from typing import Optional, Dict, List
+
+from kubernetes.client import V1Service, V1ReplicaSet, V1Pod, V1Deployment
+from kubernetes.client.rest import ApiException
+
+from k8_kat.base.kube_broker import broker
+from k8_kat.utils.main import utils
+
+LOG_REGEX = r"(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b) - - (.*)"
+
+def try_clean_log_line(line):
+  try:
+    match = re.search(LOG_REGEX, line)
+    return match.group(2) or line
+  except:
+    return line
+
+def dep_owns_pod(dep: V1Deployment, pod: V1Pod) -> bool:
+  if dep.metadata.namespace == pod.metadata.namespace:
+    dep_matchers = dep.spec.selector.match_labels
+    pod_labels = pod.metadata.labels
+    if dep_matchers is None or pod_labels is None: return False
+    return dep_matchers.items() <= pod_labels.items()
+  else:
+    return False
+
+def dep_matches_svc(dep: V1Deployment, svc: V1Service) -> bool:
+  if dep.metadata.namespace == svc.metadata.namespace:
+    dep_pod_labels = dep.spec.template.metadata.labels
+    svc_matchers = svc.spec.selector
+    if dep_pod_labels is None or svc_matchers is None: return False
+    return dep_pod_labels.items() >= svc_matchers.items()
+  else:
+    return False
+
+def dep_covers_svc_labels(svc: V1Service, labels: Dict[str, str]) -> bool:
+  svc_matchers: Dict[str, str] = svc.spec.selector
+  return svc_matchers >= labels if svc_matchers is not None else False
+
+def dep_covers_svc_pod(svc: V1Service, pod: V1Pod) -> bool:
+  pod_labels: Dict[str, str] = pod.metadata.labels
+  svc_matchers: Dict[str, str] = svc.spec.selector
+  return svc_matchers >= pod_labels if svc_matchers is not None else False
---
+++ k8_kat/utils/testing/fixtures/test_env.py
@@ -0,0 +1,74 @@
+import os
+import time
+from typing import List, Tuple
+
+from kubernetes.client import V1Namespace, V1ObjectMeta
+
+from k8_kat.base.broker_configs import default_config
+from k8_kat.base.kube_broker import broker
+from k8_kat.utils.testing.fixtures import simple_pod, simple_dep, simple_svc
+from k8_kat.utils.main import utils
+
+NAMESPACES = ['n1', 'n2', 'n3']
+
+
+def create_dep(ns: str, name: str, **subs):
+  return simple_dep.create(ns=ns, name=name, **subs)
+
+
+def create_svc(ns: str, name: str, **subs):
+  return simple_svc.create(name=name, ns=ns, **subs)
+
+
+def create_pod(ns: str, name: str, **subs):
+  return simple_pod.create(name=name, ns=ns, **subs)
+
+
+def nk_label_dep(ns: str, name: str, labels: List[Tuple[str, str]]):
+  api = broker.appsV1Api
+  dep = api.read_namespaced_deployment(namespace=ns, name=name)
+  dep.metadata.labels = {t[0]: t[1] for t in labels}
+  api.patch_namespaced_deployment(namespace=ns, name=name, body=dep)
+
+
+def k_apply(filename, **kwargs):
+  config = default_config()
+  root = utils.root_path()
+  filename = os.path.join(root, f"utils/testing/fixtures/{filename}.yaml")
+  kubectl, context = config['kubectl'], config['context']
+  utils.k_exec(f"apply -f {filename}", k=kubectl, ctx=context, **kwargs)
+
+
+def create_namespaces():
+  api = broker.coreV1
+  crt_nss = [ns.metadata.name for ns in api.list_namespace().items]
+
+  for desired_ns in NAMESPACES:
+    if desired_ns not in crt_nss:
+      api.create_namespace(
+        body=V1Namespace(metadata=V1ObjectMeta(name=desired_ns))
+      )
+
+
+def cleanup():
+  api = broker.coreV1
+  crt_nss = lambda: [_ns.metadata.name for _ns in api.list_namespace().items]
+  victim_namespaces = lambda: list(set(crt_nss()) & set(NAMESPACES))
+  for ns in victim_namespaces():
+    broker.coreV1.delete_namespace(ns)
+
+  try:
+    if len(victim_namespaces()):
+      print(f"[test_env] Waiting for ns {victim_namespaces()} to be destroyed...")
+
+    while len(victim_namespaces()):
+      time.sleep(2)
+  except Exception as e:
+    print(f"[test_env] Error {str(e)} monitoring ns destruction")
+    pass
+
+
+def terraform():
+  utils.set_run_env('test')
+  print(f"Terraforming with context {default_config()['context']}...")
+  k_apply(f"ci-perms")
