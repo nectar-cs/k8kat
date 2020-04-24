c94539705101c4a53c2eee2b2a12d80c003ac35f
xavier <xavier@codenectar.com>
2020-04-24 18:05:20 +0200

    first cloudbuild.yaml

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="k8-kat",
-  version="0.0.15",
+  version="0.0.171",
   author="Nectar Cloud Software",
   author_email="xavier@codenectar.com",
   description="Sugar for Kubernetes",
