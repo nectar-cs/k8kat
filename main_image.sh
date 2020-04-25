a97c9c09c89837795aeadc7248fb319cca399162
Xavier Millot <xavier@codenectar.com>
2020-04-25 18:16:53 +0200

    publish exits with 0

--- main_image.sh
+++ main_image.sh
@@ -4,7 +4,7 @@ if [[ "$1" == "test" ]]; then
   pipenv run python3 -m unittest
 elif [[ "$1" == "publish" ]]; then
   pipenv run python3 setup.py sdist bdist_wheel
-  pipenv run python3 -m twine upload -u xnectar -p "$PASSWORD" dist/*
+  pipenv run python3 -m twine upload -u xnectar -p "$PASSWORD" dist/*; exit 0
 else
   echo "Arg 1 must be 'test' or 'publish', was '$1'"
 fi
