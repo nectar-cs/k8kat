7207ab67e1d72fbefd0a34d5d721eb34d0adeab4
xavier <xavier@codenectar.com>
2020-06-14 19:49:54 +0200

    units.py humanize cpu/mem green

--- k8_kat/tests/utils/test_units.py
+++ k8_kat/tests/utils/test_units.py
@@ -5,16 +5,24 @@ from k8_kat.utils.main import units
 
 class TestUnits(unittest.TestCase):
 
+  def test_humanize_cpu_quant(self):
+    self.assertEqual("1.0", units.humanize_cpu_quant(1))
+    self.assertEqual("1.1 Millicores", units.humanize_cpu_quant(1.123, True))
+
+  def test_humanize_mem_quant(self):
+    self.assertEqual('15Kb', units.humanize_mem_quant(15_000))
+    self.assertEqual('15Mb', units.humanize_mem_quant(15_000_000))
+
   def test_valid_cpu_expr_to_millicores(self):
     self.assertEqual(1.5, units.quant_expr_to_bytes("1.5"))
     self.assertEqual(1.5, units.quant_expr_to_bytes("15e2m"))
     self.assertEqual(1.5, units.quant_expr_to_bytes("1500m"))
 
   def test_valid_mem_expr_to_bytes(self):
-    self.assertEqual(128974848, units.quant_expr_to_bytes("123Mi"))
-    self.assertEqual(129000000, units.quant_expr_to_bytes("129M"))
+    self.assertEqual(128_974_848, units.quant_expr_to_bytes("123Mi"))
+    self.assertEqual(129_000_000, units.quant_expr_to_bytes("129M"))
     self.assertEqual(1, units.quant_expr_to_bytes("1e3m"))
-    self.assertEqual(129000000, units.quant_expr_to_bytes("129e6"))
+    self.assertEqual(129_000_000, units.quant_expr_to_bytes("129e6"))
     self.assertEqual(1000, units.quant_expr_to_bytes("1000.0"))
 
   def test_invalid_expr_to_bytes(self):
--- k8_kat/utils/main/units.py
+++ k8_kat/utils/main/units.py
@@ -1,4 +1,3 @@
-import re
 from typing import Optional, Tuple, Dict
 
 
@@ -22,32 +21,37 @@ def quant_expr_to_bytes(expr: str) -> Optional[float]:
     return None
 
 
+def humanize_cpu_quant(millicores: float, with_unit: bool = False) -> str:
+  base = "{:.1f}".format(millicores)
+  return f"{base} {'Millicores' if with_unit else ''}".strip(' ')
+
+
+def humanize_mem_quant(byte_value: float) -> str:
+  unit_map_items = list(mem_unit_map.items())
+  sorted_items = sorted(unit_map_items, key=lambda item: item[1])
+  unit, bytes_in_unit = None, None
+  for (try_unit, multiplier) in sorted_items:
+    bytes_in_unit = byte_value / multiplier
+    if try_unit and bytes_in_unit < 1000:
+      unit = try_unit
+      break
+  return f"{int(bytes_in_unit)}{unit}b" if bytes_in_unit else None
+
+
 mem_unit_map: Dict[str, int] = {
   'Ei' : 2  ** 60,
-  'EiB' : 2  ** 60,
   'Pi' : 2  ** 50,
-  'PiB' : 2  ** 50,
   'Ti' : 2  ** 40,
-  'TiB' : 2  ** 40,
   'Gi' : 2  ** 30,
-  'GiB' : 2  ** 30,
   'Mi' : 2  ** 20,
-  'MiB' : 2  ** 20,
   'Ki' : 2  ** 10,
-  'KiB' : 2  ** 10,
   'm'  : 10 ** -3,
   'mb'  : 10 ** -3,
   'E'  : 10 ** 18,
-  'EB'  : 10 ** 18,
   'P'  : 10 ** 15,
-  'PB'  : 10 ** 15,
   'T'  : 10 ** 12,
-  'TB'  : 10 ** 12,
   'G'  : 10 ** 9,
-  'GB'  : 10 ** 9,
   'M'  : 10 ** 6,
-  'MB'  : 10 ** 6,
   'K'  : 10 ** 3,
-  'KB'  : 10 ** 3,
   ''    : 1
 }
