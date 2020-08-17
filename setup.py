a3eebac682c34efcd311782f881615a7205d980b
xavier <xavier@codenectar.com>
2020-08-17 14:33:52 +0200

    ilja broker

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="k8-kat",
-  version="0.0.155",
+  version="0.0.156",
   author="Nectar Cloud Software",
   author_email="xavier@codenectar.com",
   description="Sugar for Kubernetes",
